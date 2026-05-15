e192ccc84f7bb4abad8b284eb7f381017857a552
Léo Haf <leo@haf.ovh>
2026-05-15 14:04:17 +0200

    Update to Bitcoin Knots v29.3.knots20260508

--- build-btcknots.sh
+++ build-btcknots.sh
@@ -250,7 +250,8 @@ if [ "$USE_CMAKE" = true ]; then
     cmake_args=(
         -DBUILD_TESTS=OFF \
         -DBUILD_WALLET_TOOL=OFF \
-        -DWITH_ZMQ=OFF
+        -DWITH_ZMQ=OFF \
+        -DRDTS_CONSENT=RUNTIME_WARN
     )
 
     # If tests requested, adjust
